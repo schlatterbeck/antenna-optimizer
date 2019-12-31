53bae6e21206f229322852c3d88de2aab5ee4af8
Ralf Schlatterbeck <rsc@runtux.com>
2019-12-31 22:19:11 +0100

    New 3-element with folded dipole

--- folded_3ele.py
+++ folded_3ele.py
@@ -3,8 +3,9 @@ from __future__ import print_function
 
 from argparse import ArgumentParser
 from antenna_model import Antenna_Model, Antenna_Optimizer
+from folded        import Folded_Dipole
 
-class Folded_Dipole (Antenna_Model) :
+class Folded_Dipole_3el (Folded_Dipole) :
 
     wire_radius   = 1.5e-3 / 2.0
     dipole_radius = 0.010
@@ -13,94 +14,57 @@ class Folded_Dipole (Antenna_Model) :
     segs_arc      = 17
     segs_boom     =  5
     reflector     = 0.2
+    director      = 0.2
 
     def __init__ \
         ( self
         , refl_dist     = .01
+        , dir_dist      = .01
         , dipole_radius = dipole_radius
         , lambda_4      = lambda_4
         , reflector     = reflector
+        , director      = director
         , wire_radius   = wire_radius
         , ** kw
         ) :
-        self.refl_dist     = refl_dist
-        self.dipole_radius = dipole_radius
-        self.lambda_4      = lambda_4
-        self.reflector     = reflector
-        self.wire_radius   = wire_radius
-        self.__super.__init__ (**kw)
+        self.dir_dist      = dir_dist
+        self.director      = director
+        self.__super.__init__ \
+            ( refl_dist     = refl_dist
+            , dipole_radius = dipole_radius
+            , lambda_4      = lambda_4
+            , reflector     = reflector
+            , wire_radius   = wire_radius
+            , ** kw
+            )
     # end def __init__
 
     def cmdline (self) :
         return \
-            ("-r %(dipole_radius)1.4f -d %(refl_dist)1.4f " \
+            ("-r %(dipole_radius)1.4f -d %(refl_dist)1.4f "
+             "-D %(dir_dist)1.4f -L %(director)1.4f "
              "-l %(reflector)1.4f -4 %(lambda_4)1.4f" % self.__dict__
             )
     # end def cmdline
 
-    def geometry (self, geo = None) :
-        self.tag = 1
-        self.ex  = None
-        if geo is None :
-            geo = self.nec.get_geometry ()
-        self._geometry (geo)
-        # Turn around Y by 270 deg, move everything up
-        geo.move (0, 270, 0, 0, 0, self.up, 0, 0, 0)
-    # end def geometry
-
     def _geometry (self, geo) :
-        a = ((-90, 90), (90, 270))
-        for n, z in enumerate ((1, -1)) :
-            a1, a2 = a [n]
-            geo.arc \
-                ( self.tag
-                , self.segs_arc
-                , self.dipole_radius
-                , a1
-                , a2
-                , self.wire_radius
-                )
-            geo.move (0, 0, 0, z * self.lambda_4, 0, 0, self.tag, 0, 0)
-            self.tag += 1
-        for x in (-self.lambda_4, self.lambda_4) :
-            for z in (self.dipole_radius, -self.dipole_radius) :
-                geo.wire \
-                    ( self.tag
-                    , self.segs_dipole
-                    , 0, 0, z
-                    , x, 0, z
-                    , self.wire_radius
-                    , 1, 1
-                    )
-                self.tag += 1
-        self.ex    = self.tag - 1
-        self.exseg = 1
-        # first part of boom across folded part
+        self.__super._geometry (geo)
+        # third part of boom from dipole to director
         geo.wire \
             ( self.tag
             , self.segs_boom
-            , 0, 0,  self.dipole_radius
-            , 0, 0, -self.dipole_radius
+            , 0, 0, self.dipole_radius
+            , 0, 0, (self.dir_dist + self.dipole_radius)
             , self.wire_radius
             , 1, 1
             )
         self.tag += 1
-        # second part of boom from dipole to reflector
-        geo.wire \
-            ( self.tag
-            , self.segs_boom
-            , 0, 0, -self.dipole_radius
-            , 0, 0, -(self.refl_dist + self.dipole_radius)
-            , self.wire_radius
-            , 1, 1
-            )
-        self.tag += 1
-        # Reflector
+        # Director
         geo.wire \
             ( self.tag
             , self.segs_dipole * 3
-            , self.reflector, 0, -(self.refl_dist + self.dipole_radius)
-            , 0,              0, -(self.refl_dist + self.dipole_radius)
+            , self.director, 0, (self.dir_dist + self.dipole_radius)
+            , 0,             0, (self.dir_dist + self.dipole_radius)
             , self.wire_radius
             , 1, 1
             )
@@ -108,8 +72,8 @@ class Folded_Dipole (Antenna_Model) :
         geo.wire \
             ( self.tag
             , self.segs_dipole * 3
-            , -self.reflector, 0, -(self.refl_dist + self.dipole_radius)
-            , 0,               0, -(self.refl_dist + self.dipole_radius)
+            , -self.director, 0, (self.dir_dist + self.dipole_radius)
+            , 0,              0, (self.dir_dist + self.dipole_radius)
             , self.wire_radius
             , 1, 1
             )
@@ -118,37 +82,49 @@ class Folded_Dipole (Antenna_Model) :
 
     @property
     def up (self) :
-        """ move everything up by max (reflector length, lambda_4 + r)
-        """
-        return max (self.reflector, self.lambda_4 + self.dipole_radius)
+        return max \
+            (self.reflector, self.director, self.lambda_4 + self.dipole_radius)
     # end def up
 
-# end class Folded_Dipole
+# end class Folded_Dipole_3el
 
 class Folded_Dipole_Optimizer (Antenna_Optimizer) :
     """ Optimize given folded dipole
         Length are encoded as integers with a resolution of .5mm
         We use:
         * 8mm  <= dipole_radius <=  5cm
-        * 8mm  <= refl_dist     <= 10cm
-        * 10cm <= reflector     <= 40cm
+        * 8mm  <= refl_dist     <= 30cm
+        * 8mm  <= dir_dist      <= 30cm
+        * 10cm <= director      <= 40cm
+        * 1mm  <= refldiff      <= 15cm
         * 10cm <= lambda_4      <= 20cm
     """
 
     def __init__ (self, **kw) :
-        self.minmax = [(8e-3, 0.05), (8e-3, 0.1), (0.1, 0.4), (0.1, 0.2)]
+        self.minmax = \
+            [ (8e-3, 0.05)
+            , (8e-3, 0.3)
+            , (8e-3, 0.3)
+            , ( 0.1, 0.4)
+            , (1e-3, 0.15)
+            , ( 0.1, 0.2)
+            ]
         self.__super.__init__ (**kw)
     # end def __init__
 
     def compute_antenna (self, p, pop) :
         dipole_radius = self.get_parameter (p, pop, 0)
         refl_dist     = self.get_parameter (p, pop, 1)
-        reflector     = self.get_parameter (p, pop, 2)
-        lambda_4      = self.get_parameter (p, pop, 3)
-        fd = Folded_Dipole \
+        dir_dist      = self.get_parameter (p, pop, 2)
+        director      = self.get_parameter (p, pop, 3)
+        refldiff      = self.get_parameter (p, pop, 4)
+        lambda_4      = self.get_parameter (p, pop, 5)
+        fd = Folded_Dipole_3el \
             ( dipole_radius = dipole_radius
             , refl_dist     = refl_dist
-            , reflector     = reflector
+            , dir_dist      = dir_dist
+            , director      = director
+            , reflector     = director + refldiff
             , lambda_4      = lambda_4
             , frqidxmax     = 3
             , wire_radius   = self.wire_radius
@@ -183,10 +159,22 @@ if __name__ == '__main__' :
         , default = 0.01
         )
     cmd.add_argument \
+        ( '-D', '--director-distance'
+        , type    = float
+        , help    = "Distance of the director from nearest dipole part"
+        , default = 0.01
+        )
+    cmd.add_argument \
         ( '-i', '--frqidxmax'
         , type = int
         , help = "Number of frequency steps"
         , default = 201
+        )
+    cmd.add_argument \
+        ( '-L', '--director-length'
+        , type = float
+        , help = "(Half) Length of the director"
+        , default = 0.2
         )
     cmd.add_argument \
         ( '-l', '--reflector-length'
@@ -229,10 +217,12 @@ if __name__ == '__main__' :
         frqidxmax = args.frqidxmax
         if args.action in ('frgain', 'necout') :
             frqidxmax = 3
-        fd = Folded_Dipole \
+        fd = Folded_Dipole_3el \
             ( dipole_radius = args.dipole_radius
             , refl_dist     = args.reflector_distance
+            , dir_dist      = args.director_distance
             , reflector     = args.reflector_length
+            , director      = args.director_length
             , lambda_4      = args.lambda_len
             , wire_radius   = args.wire_radius
             , frqidxmax     = frqidxmax
